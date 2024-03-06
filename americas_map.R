a45a7565684ce1ad342f216708fad285352bbcde
Camron Pearce <camron.pearce@colostate.edu>
2024-03-06 09:50:49 -0700

    adding french guiana and updating packages

--- americas_map.R
+++ americas_map.R
@@ -2,6 +2,9 @@
 
 library(sf)
 library(rnaturalearth)
+library(rnaturalearthdata)
+#library(rnaturalearthhires) #devtools::install_github("ropensci/rnaturalearthhires") needed for F.G.
+library(ggspatial)
 library(tidyverse)
 library(scales)
 library(ggforce)
@@ -9,13 +12,13 @@ library(gridExtra)
 
 
 # Load world map data
-world <- ne_countries(returnclass = "sf") %>% 
-  filter(continent %in% c("North America", "South America") & name != "Greenland")
+# have to include europe for french guiana
+world <- ne_countries(returnclass = "sf", scale = "medium") %>% 
+  filter(continent %in% c("North America", "South America", "Europe") & name != "Greenland")
 
-
-#not ideal
 ggplot() +
-  geom_sf(data = world, fill = "lightgray", color = "black") +
+  geom_sf(data = world, fill = "#eeeee4", color = "black") +
+  geom_sf(data = french_guiana, fill = "#eeeee4", color = "black", alpha = 0.5) + # Overlay French Guiana
   geom_arc_bar(data = metadata_tidy, aes(x0 = longitude, y0 = latitude, fill = rlpm_q_pcr,
                                          r0 = 0, r = 5, 
                                          start = 0, end = 2*pi), 
@@ -29,8 +32,26 @@ ggplot() +
 #---------------
 
 ggplot() +
-  geom_sf(data = world, fill = "lightgray", color = "black") +
+  theme_bw() +
+  geom_sf(data = world, fill = "#eeeee4") +
+  geom_point(data = metadata_tidy2, aes(x = longitude, y = latitude), fill = "purple", color = "purple", size = 3) +
+  labs(title = "Map of the Americas") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         pad_x = unit(0.1, "in"), pad_y = unit(0.25, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  
+  annotation_scale(location = "bl", width_hint = 0.2) +
+  coord_sf(xlim = c(-140, -30), ylim = c(-60, 70))
+
+#-----------
+
+ggplot() +
+  geom_sf(data = world, aes(fill = pop_est, color = "black")) +
   geom_point(data = metadata_tidy2, aes(x = longitude, y = latitude), fill = "purple", color = "black", size = 3) +
   theme_void() +
   labs(title = "Map of the Americas") +
-  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"))
+  theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold")) +
+  scale_fill_viridis_c(trans = "sqrt")
